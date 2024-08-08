79b16b60e26329603a72736296091f65ad30ca56
nitesh kakkar <kakkar.nitesh04@gmail.com>
2024-08-08 23:45:09 +0530

    feat: skills tooltip

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -5,26 +5,80 @@ export default function Skills() {
     return (
         <div className="mt-20 md:px-10" id="skills">
             <p className="text-gray-900 dark:text-white text-4xl font-bold px-10">Technical Skills</p>
-            <div className="flex flex-wrap gap-10 mt-10 px-10 pr-2 md:px-10 text-gray-900 dark:text-white" >
-            <i className="devicon-python-plain text-3xl md:text-5xl"></i>
-            <i className="devicon-c-plain text-3xl md:text-5xl"></i>
-            <i className="devicon-cplusplus-plain text-3xl md:text-5xl"></i>
-            <i className="devicon-java-plain text-3xl md:text-5xl"></i>
-            <i className="devicon-html5-plain text-3xl md:text-5xl"></i>
-            <i className="devicon-css3-plain text-3xl md:text-5xl"></i>
-            <i className="devicon-javascript-plain  text-3xl md:text-5xl"></i>
-            <i className="devicon-typescript-plain text-3xl md:text-5xl"></i>
-            <i className="devicon-react-original text-3xl md:text-5xl"></i>
-            <i className="devicon-nextjs-plain text-3xl md:text-5xl"></i>
-            <i className="devicon-tailwindcss-original text-3xl md:text-5xl"></i>
-            <i className="devicon-nodejs-plain-wordmark text-3xl md:text-5xl"></i>
-            <i className="devicon-express-original-wordmark text-3xl md:text-5xl"></i>
-            <i className="devicon-mysql-original text-3xl md:text-5xl"></i>
-            <i className="devicon-postgresql-plain text-3xl md:text-5xl"></i>
-            <i className="devicon-git-plain text-3xl md:text-5xl"></i>
-            <i className="devicon-github-original text-3xl md:text-5xl"></i>
-            <i className="devicon-postman-plain text-3xl md:text-5xl"></i>
+            <div className="flex flex-wrap gap-10 mt-10 px-10 pr-2 md:px-10 text-gray-900 dark:text-white">
+                <div className="relative group">
+                    <i className="devicon-python-plain text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">Python</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-c-plain text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">C</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-cplusplus-plain text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">C++</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-java-plain text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">Java</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-html5-plain text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">HTML5</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-css3-plain text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">CSS3</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-javascript-plain text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">JavaScript</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-typescript-plain text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">TypeScript</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-react-original text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">React</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-nextjs-plain text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">Next.js</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-tailwindcss-original text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">Tailwind CSS</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-nodejs-plain-wordmark text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">Node.js</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-express-original-wordmark text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">Express.js</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-mysql-original text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">MySQL</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-postgresql-plain text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">PostgreSQL</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-git-plain text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">Git</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-github-original text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">GitHub</span>
+                </div>
+                <div className="relative group">
+                    <i className="devicon-postman-plain text-3xl md:text-5xl"></i>
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max px-2 py-1 bg-black text-white dark:bg-white dark:text-black text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">Postman</span>
+                </div>
             </div>
-      </div>
-    )
+        </div>
+    );
 }
