f0de95bc0b63eb0e9a020a271f1ba9150d1414f7
nitesh kakkar <kakkar.nitesh04@gmail.com>
2025-01-15 21:52:54 +0530

    chore: rem next themes

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,18 @@ import Header from "@/app/_components/Header";
 import Projects from "@/app/_components/Projects";
 import Skills from "@/app/_components/Skills";
 import "/public/styles.css";
-import { ThemeProvider } from "next-themes";
 import GithubActivity from "@/app/_components/GithubActivity";
 
 export default function Home() 
 {
     return (
-      <ThemeProvider attribute="class">
-        <div className="bg-gray-100 min-h-screen w-full " id="home">
-            <Header/>
-            <About/>
-            <Skills/>
-            <Projects/>
-            <GithubActivity/>
-            <Connect/>
-        </div>  
-      </ThemeProvider>
+      <div className="bg-gray-100 min-h-screen w-full " id="home">
+          <Header/>
+          <About/>
+          <Skills/>
+          <Projects/>
+          <GithubActivity/>
+          <Connect/>
+      </div>
     );
 }
