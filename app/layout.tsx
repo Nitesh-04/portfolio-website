01da501205e42c8945ce5059db3239880ec95dde
Nitesh Kakkar <kakkar.nitesh04@gmail.com>
2025-07-07 13:02:41 +0530

    add: vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "./_components/ThemeProvider";
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Nitesh Kakkar",
-  description: "Portfolio of Nitesh Kakkar",
+  description: "Portfolio Website of Nitesh Kakkar",
 };
 
 export default function RootLayout({
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider>{children}</ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
